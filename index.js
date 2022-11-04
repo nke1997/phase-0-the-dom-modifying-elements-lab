bb6120fc29b90bfed9e4b5940b5a4bb2232f96e1
Nicholas Edwards <edwards.nicholas1997@gmail.com>
2022-11-04 02:42:30 -0400

    commit tree 9bacedf85e0b5b8ee07d42b8e693728f58a14455

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+const body = document.querySelector("body");
+newHeader.id = "victory";
+newHeader.textContent = "Nicholas is the champion";
+body.appendChild(newHeader);
